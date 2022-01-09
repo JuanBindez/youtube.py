cbf0258c0fd1f472d326ab26164e98547cab20dd
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-01-09 00:10:41 -0300

    Update and rename screenLR.py to youtube.py

---
+++ youtube.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+import urllib3
+import time
+import os
+
+link = input("insira o link: ")
+
+yt = YouTube(link, on_progress_callback = on_progress)
+
+print("Titulo = ", yt.title)
+print("Baixando...")
+
+ys = yt.streams.get_highest_resolution()
+
+ys.download()
+
+os.system("clear")
+time.sleep(2)
+print("download concluido")
